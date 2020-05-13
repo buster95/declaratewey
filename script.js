f58835c71eed194b7164f2b6d59aee1f1512e0ee
Walter Corrales <corraleswalter@live.com>
2020-05-13 11:27:21 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var btnno = document.getElementById('btnno');
+
+btnno.addEventListener('mouseenter', () => {
+    console.log("mouse entrando");
+    var pos = coordenadas();
+    btnno.style.top = `${pos.posy}px`;
+    btnno.style.left = `${pos.posx}px`;
+})
+
+var coordenadas = () => {
+    var ancho = screen.width - 30;
+    var alto = screen.height - 200;
+    var posx = Math.random() * (ancho - 0) + 0;
+    var posy = Math.random() * (alto - 0) + 0;
+    return { posx, posy }
+}
